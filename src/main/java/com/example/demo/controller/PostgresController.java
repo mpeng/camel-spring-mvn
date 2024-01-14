64a4da2e58902b10d682ab78c3b0c44dacb9f4fe
wensheng.peng <wensheng.peng@cotiviti.com>
2024-01-14 15:58:08 -0500

    Add classes related to PostgreSQL

--- src/main/java/com/example/demo/controller/PostgresController.java
+++ src/main/java/com/example/demo/controller/PostgresController.java
@@ -20,16 +20,19 @@ import java.sql.SQLException;
 
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api2")
 public class PostgresController {
 
   // JDBC URL, username, and password of PostgreSQL server
-  private static final String JDBC_URL = "jdbc:postgresql://your_database_host:your_database_port/your_database_name";
-  private static final String USERNAME = "your_database_username";
-  private static final String PASSWORD = "your_database_password";
+  private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/testdb";
+  private static final String USERNAME = "postgres";
+  private static final String PASSWORD = "123";
+
   @GetMapping("/postgres")
   @ResponseStatus(HttpStatus.OK)
   public @ResponseBody String postgres() {
+
+    String response = "OK";
 
       try {
         // Load the PostgreSQL JDBC driver
@@ -41,29 +44,41 @@ public class PostgresController {
             System.out.println("Connected to the PostgreSQL server.");
 
             // Query to retrieve data from the coordinates table
-            String query = "SELECT * FROM coordinates";
+            String query = "SELECT * FROM coordiantes";
 
             // Prepare and execute the query
+            System.out.println( "Here1" );
             try (PreparedStatement preparedStatement = connection.prepareStatement(query);
                  ResultSet resultSet = preparedStatement.executeQuery()) {
 
+              System.out.println( "Here2" );
+
               // Process the result set
               while (resultSet.next()) {
+                System.out.println( "Here3" );
                 int id = resultSet.getInt("id");
-                double x = resultSet.getDouble("location.x");
-                double y = resultSet.getDouble("location.y");
+                System.out.println( "Here4" );
+                Object x = resultSet.getObject("location");
+                System.out.println( "Here5" );
+                Object y = resultSet.getObject("location");
+                System.out.println( "Here6" );
 
-                System.out.println("ID: " + id + ", X: " + x + ", Y: " + y);
+                response = String.format( "ID: %d, X: %s, Y: %s", id, x, y );
+                System.out.println( "Here7" );
+                System.out.println( response );
+                System.out.println( "Here8" );
               }
             }
           } else {
+            System.out.println( "Here9" );
             System.out.println("Failed to connect to the PostgreSQL server.");
           }
         }
-        return "Ok";
+        System.out.println( "Here10" );
+        return response;
       } catch (ClassNotFoundException | SQLException e) {
+        System.out.println( "Here11: " + e );
         throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
       }
-    }
   }
 }
